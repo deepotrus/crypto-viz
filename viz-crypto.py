bf27705906b80fadd900be40a89b79606d806420
Andrei Potra <88773087+deepotrus@users.noreply.github.com>
2023-10-16 15:24:35 +0000

    Update viz-crypto.py

--- viz-crypto.py
+++ viz-crypto.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-pip install tvdatafeed
-from tvDatafeed import TvDatafeed, Interval
+from tvdatafeed import TvDatafeed, Interval
 
 from bokeh.plotting import figure, show, output_file
 from bokeh.io import output_notebook
